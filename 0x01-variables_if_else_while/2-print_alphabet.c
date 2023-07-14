d369a38d963b29d378c562704cb08b9de2c4bf2f
lujai <lujainben@gmail.com>
2023-07-14 02:36:38 -0700

    iam perfect

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/** 
+ * jhd
+ * gfkdj
+ * gfk
+ */
+int main(void)
+{
+	char s[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+	return (0);
+}
