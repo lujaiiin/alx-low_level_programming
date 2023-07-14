969672a0c463e72db4ec59c9e81a64608e40486b
lujai <lujainben@gmail.com>
2023-07-14 08:20:54 -0700

    iam perfect

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Entry point
+*Return: always 0
+*/
+int main(void)
+{
+	int b, d;
+
+	for (b = 0; b <= 98; b++)
+	{
+		for (d = b + 1; d <= 99; d++)
+		{
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			putchar(' ');
+			putchar((d / 10) + '0');
+			putchar((d % 10) + '0');
+			if (b == 98 && d == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
