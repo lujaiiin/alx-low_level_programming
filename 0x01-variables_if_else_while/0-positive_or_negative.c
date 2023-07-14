6ca317c21d886e4960a6c519e7ec1bf5397fc7e2
lujai <lujainben@gmail.com>
2023-07-13 23:46:09 -0700

    iam perfect

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*gf,jzf,
+ * h,dfdjh
+ * fgdyslfl
+ * sgfl
+ * fdg
+*/
+int main(void)
+{
+	int n;
+	printf("enter your num");
+	scanf("%d" , &n);
+	if(n > 0){
+		printf("is positive");
+	}else if(n == 0){
+		printf("is zero");
+	}else{
+		printf("is negative");
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+if and if else statments 
