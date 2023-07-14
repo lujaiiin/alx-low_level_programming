fd4ca42e4bd2e1a0edaead9e9ab3d73cf455a567
lujai <lujainben@gmail.com>
2023-07-14 02:52:38 -0700

    iam perfect

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
-/** 
- * jhd
- * gfkdj
- * gfk
+
+/**
+ * main - prints the alphabet
+ * 
+ * Return always (0)
  */
 int main(void)
 {
 	char s[26] = "abcdefghijklmnopqrstuvwxyz";
+	char l[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
+
 	for (i = 0; i < 26; i++)
 	{
 		putchar(s[i]);
+		putchar(l[i]);
 	}
 	putchar('\n');
 	return (0);
