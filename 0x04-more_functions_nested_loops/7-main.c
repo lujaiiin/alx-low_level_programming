742d52be39a74b0d1f0851f0854d94d3c294c789
lujai <lujainben@gmail.com>
2023-07-20 09:53:26 -0700

    hi

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-    print_diagonal(0);
-    print_diagonal(2);
+    print_diagonal(-9);
+    print_diagonal(4);
     print_diagonal(10);
     print_diagonal(-4);
     return (0);
