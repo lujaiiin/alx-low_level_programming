95f4328bd6024793d509704a05a727add07ce0dd
lujai <lujainben@gmail.com>
2023-07-20 16:06:51 -0700

    iam perfect

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,7 +9,7 @@
 void print_number(int n)
 {
 	unsigned int o;
-	
+
 	o = n;
 	if (n < 0)
 	{
@@ -18,7 +18,7 @@ void print_number(int n)
 	}
 	if (o / 10)
 	{
-		_putchar(o / 10 + '0');
+		print_number(o / 10);
 	}
 	_putchar(o % 10 + '0');
 }
