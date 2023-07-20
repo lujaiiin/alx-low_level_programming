e07f2db4a68044256d5b57141f0db9df9c19947b
lujai <lujainben@gmail.com>
2023-07-20 07:17:56 -0700

    iam perfect

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *mul - function well mult the values
+ *
+ * @a: a value i well mult it
+ *
+ * @b: a value i well mult it too
+ *
+ * Return: No (succes)
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
