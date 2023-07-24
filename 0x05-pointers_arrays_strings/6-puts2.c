1d52799e390ca922c3bdb833f83182913f52abab
lujai <lujainben@gmail.com>
2023-07-24 13:18:30 -0700

    lalalalala

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,9 +11,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i]!= '\0'; i += 2)
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
-		_putchar(str[i]);
+		printf("%c", str[i]);
 	}
-	_putchar('\n');
+	printf("\n");
 }
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,5 +3,36 @@
 /**
  *puts_half - function puts half
  *
+ *@str: value
+ */
+
+void puts2(char *str)
+{
+	int l, s, i;
+
+	l = _strlen(str);
+	s = (l + 1) / 2;
+
+	for (i = s; i < l; i++)
+	{
+		printf("%c", str[i]);
+	}
+	printf("\n");
+}
+
+/**
+ *_strlen: function
  *
  */
+
+int _strlen(char *s)
+{
+	int ad = 0;
+
+	while (*s != '\0')
+	{
+		ad++;
+		s++;
+	}
+	return (ad);
+}
