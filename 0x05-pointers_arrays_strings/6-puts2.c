b931998b4916bbb8a6fd69afbda7da19115c337c
lujai <lujainben@gmail.com>
2023-07-24 13:34:23 -0700

    lalalalala

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,8 +10,14 @@
 void puts2(char *str)
 {
 	int i;
+	int k = 0;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (str[k] != '\0')
+	{
+		k++;
+	}
+
+	for (i = 0; i < k; i += 2)
 	{
 		printf("%c", str[i]);
 	}
