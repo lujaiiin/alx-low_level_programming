02c0a1f1dca281743edc3e75f0d3219cea7dc6a8
lujai <lujainben@gmail.com>
2023-07-24 01:06:48 -0700

    iam perfect

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - function give me a pointer
+ *
+ *@n: value that i well see the pointer
+ *
+ *Return: No (succes)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
