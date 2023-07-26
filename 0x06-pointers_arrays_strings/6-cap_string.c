3568bb90b6760e7528c9b0e44e7698d506c4be62
lujai <lujainben@gmail.com>
2023-07-26 16:29:23 -0700

    lalalalala

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,15 +10,28 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == 46 )
+		while (str[i] <= 'a' && str[i] >= 'z')
+			i++;
+		if (str[i] == ' ' ||
+				str[i] == '\n' ||
+				str[i] == '\t' ||
+				str[i] == 46 ||
+				str[i] == ',' ||
+				str[i] == ';' ||
+				str[i] == '{' ||
+				str[i] == '}' ||
+				str[i] == '!' ||
+				str[i] == '?' ||
+				str[i] == '(' ||
+				str[i] == ')' ||
+				str[i] == '"')
 		{
-			str[i + 1] += 32;
+			str[i + 1] -= 32;
 		}
 		i++;
 	}
-	str[i] = '\0';
 
 	return (str);
 }
