fc728b18d4bcb91eead4886531c287c77f332d24
lujai <lujainben@gmail.com>
2023-07-27 03:53:44 -0700

    lalalalala

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *_leet - function
+ *
+ *@s: value
+ *Return: always
+ */
+char *leet(char *s)
+{
+	int i, a;
+	char l[] = {"aeotl"};
+	char n[] = {"AEOTL"};
+	char o[] = {"43071"};
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		for (a = 0; a <= 5; a++)
+		{
+		if (s[i] == l[a] || s[i] == n[a])
+		{
+			s[i] = o[a];
+		}
+		}
+		i++;
+	}
+	return (s);
+
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,3 +8,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *rot13(char *);
 char *cap_string(char *);
+char *leet(char *);
