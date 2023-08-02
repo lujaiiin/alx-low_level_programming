80d893b9f283c58e07904e0faf8817671bd94b15
lujai <lujainben@gmail.com>
2023-08-02 13:26:07 -0700

    lalalalala

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,51 +1,60 @@
 #include "main.h"
 
 /**
- *str - function
- *@s: value
- *Return: always
- */
-
-int str(char *s)
-{
-	if (*s == '\0')
-	{
-		return (0);
-	}
-	else
-	{
-		return (1 + str(s + 1));
-	}
-}
-
-/**
- *com - function
- *@s: value
- *@l: valie
- *@r: value
- *Return: always
- */
-
-int com(char *s, int l, int r)
-{
-	if (*(s + l) == *(s + r))
-	{
-		if (l == r || l == r + 1)
-			return (1);
-		return (0 + com(s, l + 1, r - 1));
-	}
-	return (0);
-}
-
-/**
  *is_palindrome - function
+ *all - function
+ *add - function
  *@s: value
- *Return: return
+ *@st: value
+ *@ls: value
+ *@mo: value
+ * Return: always
  */
+
+int all(char *s, int st, int ls, int mo);
+int add(char *s);
 
 int is_palindrome(char *s)
 {
-	if (*s == '\0')
+	int e = add(s);
+
+	return (all(s, 0, e - 1, e % 2));
+}
+
+/**
+ *add - function
+ *@s: value
+ *Return: always
+ */
+
+int add(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i += add(s + 1) + 1;
+	}
+	return (i);
+}
+
+/**
+ *all - function
+ *@s: value
+ *@st: value
+ *@ls: value
+ *@mo: value
+ *Return: always
+ */
+
+int all(char *s, int st, int ls, int mo)
+{
+	if ((st == ls && mo % 2 != 0) || (st == ls + 1 && mo % 2 == 0))
+	{
 		return (1);
-	return (com(s, 0, str(s - 1)));
+	}
+	if (s[st] != s[ls])
+		return (0);
+	else
+		return (all(s, st + 1, ls - 1, mo));
 }
