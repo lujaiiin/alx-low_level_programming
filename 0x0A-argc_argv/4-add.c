87a3d7c2c910cee8fd844d10ac22c12cb102e79e
lujai <lujainben@gmail.com>
2023-08-04 15:56:40 -0700

    lalalalala

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - function
+ *@argv: value
+ *@argc: value
+ *Return: always
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned int s;
+	int x;
+
+	for (x = 1; x < argc; x++)
+	{
+		if (*argv[x] <= 57 && *argv[x] >= 48)
+		{
+			if (atoi(argv[x]) > 0)
+				s += atoi(argv[x]);
+		}
+		else if (*argv[x] > 57 || *argv[x] < 48)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", s);
+	return (0);
+}
