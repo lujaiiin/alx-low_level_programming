9b65d7756d4f3a89df8e5b7f748c886bb412afda
lujai <lujainben@gmail.com>
2023-08-04 14:55:31 -0700

    lalalalala

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - function
+ *@argc: value
+ *@argv: value
+ *Return: always
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
