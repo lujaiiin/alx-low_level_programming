a206dcea1449de9cbeaacb8d23a1664e80071b9f
lujai <lujainben@gmail.com>
2023-08-04 14:49:50 -0700

    lalalalala

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - function
+ *@argv: value
+ *@argc: value
+ *Return: always
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
