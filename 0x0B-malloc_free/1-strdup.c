3a4a757ba3adaa62807674fdcb9dc306e85dad0f
lujai <lujainben@gmail.com>
2023-08-07 07:09:34 -0700

    lalalalala

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - function
+ *@str: value
+ *Return: always
+ */
+
+int lenght(char *str);
+
+char *_strdup(char *str)
+{
+	char *p;
+	int i;
+
+	p = malloc(sizeof(char) * lenght(str));
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < lenght(str); i++)
+		{
+			p[i] = str[i];
+		}
+	}
+	return (p);
+}
+
+/**
+ *lenght - function
+ *@str: value
+ *Return: always
+ */
+
+int lenght(char *str)
+{
+	int i = 0;
+	int ad = 0;
+
+	while (str[i] != '\0')
+	{
+		ad++;
+		i++;
+	}
+	return (ad);
+}
