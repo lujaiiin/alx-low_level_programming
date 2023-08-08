531d4a66912d57daec2a8eb6eed94f42aa142b35
lujai <lujainben@gmail.com>
2023-08-08 07:58:52 -0700

    lalalalala

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int co(char *str);
+int lenght(char *str);
+
 /**
  *strtow - function
  *@str: value
@@ -10,34 +13,93 @@
 
 char **strtow(char *str)
 {
-	char *p;
-	int i;
+	char **p;
+	int i = 0;
+	int wo, y, le, l;
 
-	i = 0;
-
-	if (str == NULL || str == "")
+	if (str == NULL || str[0] == '\0')
 	{
 		return (NULL);
 	}
 
-	p = malloc(sizeof(char) * );
+	wo = co(str);
+	if (wo == 0)
+		return (NULL);
 
+	p = malloc(sizeof(char*) *(wo + 1) );
 	if (p == NULL)
 	{
 		return (NULL);
 	}
 
-	while (str[i] != '\0')
+	for (y = 0; y < wo; y++)
 	{
-		if (str[i] >= 65 || str[i] <= 90 || str[i] >= 97 || str[i] <= 122)
+		while (str[i] == ' ')
+			i++;
+
+		le = lenght(str + i);
+
+		p[y] = malloc(sizeof(char) * (le + 1));
+
+		if (p[y] == NULL)
 		{
-			p[i] = str[i];
-			if (str[i] == " ")
-			{
-				continue;
-			}
+			for (; y >= 0; y--)
+				free(p[y]);
+			free(p);
+			return (NULL);
 		}
+		for (l = 0; l < le; l++)
+			p[y][l] = str[i++];
+
+		p[y][l] = '\0';
+	}
+	p[y] = NULL;
+
+	return (p);
+}
+
+/**
+ *lenght - function
+ *@str: value
+ *Return: always
+ */
+
+int lenght(char *str)
+{
+	int i = 0;
+	int ad = 0;
+
+	while (*(str + i) && *(str + i) != ' ')
+	{
+		ad++;
 		i++;
 	}
-	return (p);
+	return (ad);
+}
+
+/**
+ *co - function
+ *@str: value
+ *Return: always
+ */
+
+int co(char *str)
+{
+	int i = 0;
+	int w = 0;
+	int l = 0;
+
+	for (i = 0; *(str + i); i++)
+	{
+		l++;
+	}
+	for (i = 0; i < l; i++)
+	{
+		if (*(str + i) != ' ')
+		{
+			w++;
+			i += lenght(str + i);
+		}
+	}
+	return (w);
 }
