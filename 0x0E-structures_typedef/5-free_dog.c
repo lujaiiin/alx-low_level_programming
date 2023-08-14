3213f9c90ee889146835b7a47ad77cb985aa86cc
lujai <lujainben@gmail.com>
2023-08-14 09:40:24 -0700

    lalalalala

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function
+ * @d: value
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
