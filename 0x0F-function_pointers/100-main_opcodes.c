5d181a72cc6976848682a0fa762982889f4fe383
lujai <lujainben@gmail.com>
2023-08-16 15:01:27 -0700

    lalalalala

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - function
+ *@argc: value
+ *@argv: value
+ *Return: always
+ */
+
+int main(int argc, char **argv)
+{
+	char *ptr = (char *)main;
+	int p;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit (1);
+	}
+	p = atoi(argv[1]);
+	if (p < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	while (p--)
+	{
+		printf("%02hhx%s", *ptr++, p ? " " : "\n");
+	}
+	return (0);
+}
