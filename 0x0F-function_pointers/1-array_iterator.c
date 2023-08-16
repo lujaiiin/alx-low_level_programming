80bcc5f379c528947f95f907b591c6279e79a787
lujai <lujainben@gmail.com>
2023-08-16 12:06:07 -0700

    lalalalala

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ *array_iterator  - function
+ *@array: value
+*@action: value
+ *@size: value
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL && size != 0 && action != NULL)
+	{
+		for (i = 0; i <= size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
