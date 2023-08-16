9ab72b1308e458a55ffb863b458975198d0daec7
lujai <lujainben@gmail.com>
2023-08-16 14:26:17 -0700

    lalalalala

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - function
- * argc: value
+ * @argc: value
  * @argv: value
  * Return: always
  */
@@ -14,16 +14,17 @@ int main(int argc, char *argv[])
 	int an;
 
 	s = argv[2];
-	if(argc != 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if (s[1] != '\0' || (*s != '+' && *s != '-' && *s != '/' && *s != '%' && *s != '*'))
+	if (s[1] != '\0' || (*s != '+' && *s != '-' && *s != '/'
+				&& *s != '%' && *s != '*'))
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 	if (atoi(argv[3]) == 0 && (*s == '%' || *s == '/'))
 	{
