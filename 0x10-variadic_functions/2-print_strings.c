783af6c9d2c9259313c649f7f0f965678fa5e8b7
lujai <lujainben@gmail.com>
2023-08-17 09:38:02 -0700

    lalalalala

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		x = va_arg(args, char*);
 		if (x == NULL)
-			printf("nil");
+			printf("(nil)");
 		else
 			printf("%s", x);
 		if (i + 1 == n)
