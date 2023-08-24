d3d19a3658e243d71905ee0e0b7ce69fd3a8e0db
lujain <lujainm.com>
2023-08-24 10:04:56 -0700

    lalalalala

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * add_node - function
+ * @head: value
+ * @str: value
+ * Return: always
+ */
+
+int lenght(char *l)
+{
+	int ad = 0;
+	int i = 0;
+
+	while (l[i] != '\0')
+	{
+		i++;
+		ad++;
+	}
+	return (ad);
+}
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newn;
+
+	newn = malloc(sizeof(list_t));
+	if (newn == NULL || head == NULL)
+	{
+		return (NULL);
+	}
+	if (str != NULL)
+	{
+		newn->str = strdup(str);
+		if (newn->str == NULL)
+		{
+			free(newn);
+			return (NULL);
+		}
+		newn->len = lenght(newn->str);
+	}
+	newn->next = *head;
+	*head = newn;
+	return (newn);
+}
