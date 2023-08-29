b7daa6f471a555260cac87e165d23f7c0dcebd95
lujain <lujainm.com>
2023-08-29 10:03:49 -0700

    lalalalala

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function
+ * @head: value
+ * Return: always
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *p;
+	int i = 0;
+
+	while (*head != NULL)
+	{
+		i = (*head)->n;
+		p = (*head)->next;
+		free(*head);
+		*head = p;
+	}
+	return (i);
+}
