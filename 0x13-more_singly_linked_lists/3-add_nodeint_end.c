742f46000a5ecb08f9fa30e5196fdb1f1e69aa65
lujain <lujainm.com>
2023-08-29 04:05:16 -0700

    lalalalala

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,6 +15,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	p = malloc(sizeof(listint_t));
 
+	if (head == NULL || p == NULL)
+	{
+		return (NULL);
+	}
 	p->n = n;
 	p->next = NULL;
 
