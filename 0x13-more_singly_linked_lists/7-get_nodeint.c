a7eeb578418ddf6dd83552520945e9ace2227a4c
lujain <lujainm.com>
2023-08-29 14:12:42 -0700

    lalalalala

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - function
+ * @head: value
+ * @index: value
+ * Return: always
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (i < index)
+	{
+		head = head->next;
+		i++;
+	}
+	if (i != index || head == NULL)
+	{
+		return (NULL);
+	}
+
+	return (head);
+}
