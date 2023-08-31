0f30d6fe4e83583f3635e5401cc0694991167090
lujain <lujainm.com>
2023-08-31 07:46:25 -0700

    lalalalala

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+unsigned int lenght(const char *s);
+unsigned int powe(int a, unsigned int b);
+/**
+ * binary_to_uint - function
+ * @b: value
+ * Return: always
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int  l = 0, s = 0, i = 0;
+
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while (b[i] != '\0')
+	{
+		if ((b[i] >= 65 && b[i] <= 90) || (b[i] <= 122 && b[i] >= 97))
+		{
+			return (0);
+		}
+		else
+		{
+			s = b[(i)] - 48;
+			l += (s * powe(2, i));
+		}
+		i++;
+	}
+	return (l);
+}
+
+/**
+ * powe - function
+ * @a: value
+ * @b: value
+ * Return: always
+ */
+
+unsigned int powe(int a, unsigned int b)
+{
+	unsigned int i, sum = 1;
+
+	if (b == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		for (i = 0; i < b; i++)
+		{
+			sum *= a;
+		}
+	}
+	return (sum);
+}
+
+/**
+ * lenght -= function
+ * @s: valuw
+ * Return: always
+ */
+
+unsigned int lenght(const char *s)
+{
+	unsigned int i = 0, ad = 0;
+
+	while (s[i] != '\0')
+	{
+		ad++;
+		i++;
+	}
+	return (ad);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+Bit manoulation
