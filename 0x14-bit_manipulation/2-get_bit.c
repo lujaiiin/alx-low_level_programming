4df9f097a18e1c3867d373942ee8701664e7ce5d
lujain <lujainm.com>
2023-08-31 11:05:50 -0700

    lalalalala

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_binary - function
+ *@n: valie
+ *Return: always
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i = 0;
+	unsigned int j;
+
+	if (index > 63)
+	{
+		return (-1);
+	}
+	else
+	{
+		j = ( n >> index);
+		i = 1 & j;
+	}
+	return (i);
+}
