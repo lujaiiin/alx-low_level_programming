8e7251d74fcf3fc7dd7b6b7837ac97ddb7fbaccb
lujain <lujainm.com>
2023-09-04 07:47:01 -0700

    lalalalala

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,17 +15,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char buf[1024];
 	ssize_t b;
 
-	if (filename == NULL)
+	if (filename == NULL || !letters)
 	{
 		return (0);
 	}
 	else
 	{
 		file = open(filename, O_RDONLY);
-		if (!letters)
-		{
-			return (0);
-		}
 		if (file == -1)
 		{
 			return (0);
@@ -34,10 +30,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		{
 			r = read(file, &buf[0], letters);
 			b = write(STDOUT_FILENO, &buf[0], r);
-			if (!b)
-			{
-				return (0);
-			}
 		}
 	}
 	close(file);
