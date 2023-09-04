ba874e4e8d998de9d1fef3ab67c5494240fe2be9
lujain <lujainm.com>
2023-09-04 13:07:20 -0700

    lalalalala

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - function
+ * @filename: value
+ * @text_content: value
+ * Return: always
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int f1, i = 0, r;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	f1 = open(filename, O_WRONLY | O_APPEND);
+	if (f1 == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		while (text_content[i])
+		{
+			i++;
+		}
+		r = write(f1, text_content, i);
+	}
+	close(f1);
+	if (r != i)
+	{
+		return (-1);
+	}
+	return (1);
+}
