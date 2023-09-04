a5c675d6d2c292da831bfe657b254c287af25c8c
lujain <lujainm.com>
2023-09-04 16:49:08 -0700

    lalalalala

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,7 +5,30 @@
 
 #define ER STDERR_FILENO
 #define US "Usage: elf_header %s\n"
+/**
+ * ent - function
+ * @e: value
+ * @en: value
+ * Return: value
+ */
 
+void ent(unsigned long int e, unsigned char *en)
+{
+	printf("  Entry point address:               ");
+
+	if (en[EI_DATA] == ELFDATA2MSB)
+	{
+		e = ((e << 8) & 0xFF00FF00) |
+			((e >> 8) & 0xFF00FF);
+		e = (e << 16) | (e >> 16);
+	}
+
+	if (en[EI_CLASS] == ELFCLASS32)
+	{
+		printf("%#x\n", (unsigned int)e);
+	}
+	printf("%#lx\n", e);
+}
 /**
  * EL - function
  * @E: value
@@ -53,6 +76,28 @@ void cl(unsigned char *c)
 }
 
 /**
+ * ve - function
+ * @v: value
+ * Return: always
+ */
+
+void ve(unsigned char *v)
+{
+	printf("  Version:                           %d", v[EI_VERSION]);
+
+	switch (v[EI_VERSION])
+	{
+		case EV_CURRENT:
+			printf(" (current)\n");
+			break;
+		default:
+			printf("\n");
+			break;
+	}
+
+}
+
+/**
  * da - function
  * @d: value
  * Return: always
@@ -76,12 +121,31 @@ void da(unsigned char *d)
 		printf("2's complement, little endian\n");
 	}
 }
+
+/**
+ * ty - function
+ * @t: value
+ * @te: value
+ * Return: always
+ */
+
+void ty(unsigned int t, unsigned char *te)
+{
+	if (te[EI_DATA] == ELFDATA2MSB)
+	{
+		t >>= 8;
+	}
+	printf("  Type:                              ");
+	if (t == ET_NONE)
+	{
+		printf("NONE (None)\n");
+	}
+}
 /**
  * ma - function
  * @m: value
  * Return: always
  */
-
 void ma(unsigned char *m)
 {
 	int i;
@@ -109,19 +173,22 @@ int main(int c, char *v[])
 	Elf64_Ehdr *E;
 
 	if (c != 2)
-	{
-		dprintf(ER, US, v[1]);
-		exit(98);
-	}
+		dprintf(ER, US, v[1]), exit(98);
 	f1 = open(v[1], O_RDONLY);
 	if (f1 == -1)
 	{
-		dprintf(ER, "cant open: %s\n", v[1]);
+		dprintf(ER, "cant open: %s\n", v[1]), exit(98);
+	}
+	E = malloc(sizeof(Elf64_Ehdr));
+	if (E == NULL)
+	{
+		close(f1);
 		exit(98);
 	}
-	r = read(f1, &E, sizeof(E));
-	if (r < 1 || r != sizeof(E))
+	r = read(f1, E, sizeof(Elf64_Ehdr));
+	if (r < 1)
 	{
+		free(E);
 		dprintf(ER, "cant read: %s\n", v[1]);
 		close(f1);
 		exit(98);
@@ -130,6 +197,8 @@ int main(int c, char *v[])
 	ma(E->e_ident);
 	cl(E->e_ident);
 	da(E->e_ident);
+	ve(E->e_ident);
+	ent(E->e_entry, E->e_ident);
 	l = close(f1);
 	if (l == -1)
 	{
