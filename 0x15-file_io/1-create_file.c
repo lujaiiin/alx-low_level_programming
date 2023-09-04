0cb773302aee16578a82d70603916e8c4ea7d6d9
lujain <lujainm.com>
2023-09-04 12:12:31 -0700

    lalalalala

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,8 @@ int create_file(const char *filename, char *text_content)
 		{
 			i++;
 		}
-		r = write(f1, text_content, i);
+		if (i)
+			r = write(f1, text_content, i);
 	}
 	close(f1);
 	if (i != r)
