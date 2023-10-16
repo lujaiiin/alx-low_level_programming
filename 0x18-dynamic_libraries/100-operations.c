59d07efd5eaa7c944126933edc0c503407a38529
lujain <lujainm.com>
2023-10-16 11:59:45 -0700

    Dynamic lib

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int mod(int x, int y)
+{
+	if (y == 0)
+	{
+		printf("Error\n");
+		return (0);
+	}
+}
+int mul(int x, int y)
+{
+	return (x * y);
+}
+
+int div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	return (a / b);
+}
